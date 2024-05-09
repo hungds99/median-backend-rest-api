5b70c84e546ba83632409138a69d04308953de62
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-05-09 11:41:23 +0700

    chore: add region

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,15 +1,17 @@
-import { SecretsManager } from '@aws-sdk/client-secrets-manager';
+// import { SecretsManager } from '@aws-sdk/client-secrets-manager';
 
 export default async () => {
-  const secretsManager = new SecretsManager();
+  // const secretsManager = new SecretsManager({
+  //   region: 'ap-southeast-1',
+  // });
 
-  const secretDatabaseUrl = await secretsManager.getSecretValue({
-    SecretId: 'SUPABASE_POSTGRES_DB_URL',
-  });
+  // const secretDatabaseUrl = await secretsManager.getSecretValue({
+  //   SecretId: 'SUPABASE_POSTGRES_DB_URL',
+  // });
 
-  const databaseUrl = JSON.parse(secretDatabaseUrl.SecretString);
+  // const databaseUrl = JSON.parse(secretDatabaseUrl.SecretString);
 
   return {
-    DATABASE_URL: databaseUrl,
+    // DATABASE_URL: "",
   };
 };
